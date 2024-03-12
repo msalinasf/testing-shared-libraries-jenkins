dcf8f3bada59c2dcbd3afecc88f6517bd213dd71
Marcos Salinas <38986037+msalinasf@users.noreply.github.com>
2024-03-12 14:49:52 +0100

    Update git.groovy

--- vars/git.groovy
+++ vars/git.groovy
@@ -44,31 +44,3 @@ def merge(main, secondary = '') {
     echo "Merge ${secondary} into ${main}"
     """
 }
-
-    echo "Comenzamos ejecución $first_time_execution"
-    if ( first_time_execution ) {
-        sh """
-        echo "Primera vez que ejecutamos con la rama $branch"
-        """
-        first_time_execution = false
-    }
-    echo "Finalizamos ejecución $first_time_execution"
-}
-
-def checkout(branch) {
-    sh """
-    #!/bin/sh -xe
-    echo "Nombre de la rama $branch"
-    """
-}
-
-def merge(main, secondary = '') {
-    sh """
-    #!/bin/sh -xe
-    if [ -z "${secondary}" ]; then
-        echo "No branch found. Finishing stage."
-        exit 0
-    fi
-    echo "Merge ${secondary} into ${main}"
-    """
-}
