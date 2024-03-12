8ed42d481f108a3fd26d4aa359493f42f6a1c88a
Marcos Salinas <38986037+msalinasf@users.noreply.github.com>
2024-03-12 08:24:19 +0100

    Update git.groovy

--- vars/git.groovy
+++ vars/git.groovy
@@ -1,9 +1,11 @@
 def checkout(branch) {
     // echo "Bye, ${branch}."
+    echo "begin"
     sh '''
     #!/bin/sh -xe
     # get hotfix branch
     # git checkout ${branch}
     echo ${branch}
     '''
+    echo "end"
 }
