839fbfadaa94bddd42632ba8ad92e00faf3751ab
Marcos Salinas <38986037+msalinasf@users.noreply.github.com>
2024-04-12 11:58:10 +0200

    Create log.groovy

---
+++ vars/log.groovy
@@ -0,0 +1,25 @@
+def loadColors() {
+    RED= '31'
+    BLUE= '34'
+    CYAN= '36'
+    GREEN= '32'
+    MAGENTA='35'
+    YELLOW= '33'
+    LIGHT_MAGENTA='95'
+    LIGHT_YELLOW= '93'
+    ON_YELLOW= '43'
+    ON_GREEN= '42'
+    ON_LIGHT_MAGENTA= '105'
+    ON_BLUE= '44'
+    ON_LIGHT_BLUE= '104'
+    NORMAL= '0'
+    BOLD= '1'
+    PRE= '\033['
+    POST= 'm'
+}
+
+def infoStage() {
+    loadColors()
+    date = date()
+    sh"""set +x; echo "${PRE}${BOLD};${BLUE}${POST} [STAGE] - $date - ==================================== HOLAAAAA ========================================================== " """
+}
