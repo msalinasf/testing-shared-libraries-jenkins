8dfd9e1bebfcab1714ae8760bff5597bc3e92563
Marcos Salinas <38986037+msalinasf@users.noreply.github.com>
2024-04-18 10:32:46 +0200

    Update log.groovy

--- vars/log.groovy
+++ vars/log.groovy
@@ -27,5 +27,5 @@ def date(){
 def infoStage() {
     loadColors()
     date = date()
-    sh"""echo "${PRE}${BOLD};${BLUE}${POST} [STAGE] - $date - ==================================== HOLAAAAA ========================================================== ${PRE}${NORMAL}${POST}" """
+    sh"""set +x; echo "${PRE}${BOLD};${BLUE}${POST} [STAGE] - $date - ==================================== HOLAAAAA ========================================================== ${PRE}${NORMAL}${POST}"; set -x;"""
 }
